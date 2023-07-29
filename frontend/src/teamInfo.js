9e3a974c92420c173eac626f89e7b2977eff3b1d
Zachary <tysonzachary@ufl.edu>
2023-07-29 16:15:56 -0400

    updated config for starting side of team one

--- frontend/src/teamInfo.js
+++ frontend/src/teamInfo.js
@@ -3,9 +3,13 @@ import teamTwoLogo from "./config/teamTwoLogo.png";
 import cfg from "./config/config.json"
 const teamOneName = cfg.teamOne;
 const teamTwoName = cfg.teamTwo;
+
+const teamOneStartingSide = cfg.teamOneStartingSide;
+
 export {
   teamOneLogo,
   teamTwoLogo,
   teamOneName,
   teamTwoName,
+  teamOneStartingSide,
 };
