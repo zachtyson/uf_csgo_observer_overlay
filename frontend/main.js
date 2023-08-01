cef7cf3de194f220e6d3c345eb619c11c192bd21
Zachary <tysonzachary@ufl.edu>
2023-07-31 23:31:16 -0400

    function to get config data

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,55 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron')
 const path = require('path');
 const url = require('url');
 const express = require('express');
+const fs = require('fs');
 
 let server;
 let mainWindow;
 
-app.on('ready', function () {
+
+
+function readConfigFile() {
+    try {
+        const configFileContent = fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8');
+        const config = JSON.parse(configFileContent);
+        return config;
+    } catch (err) {
+        console.error('Error reading config file:', err);
+        return null;
+    }
+}
+
+async function storeImageAsVariable(filePath) {
+    try {
+        const imageData= await fs.promises.readFile(filePath);
+        return imageData.toString('base64');
+    } catch (error) {
+        console.error('Error reading the image file:', error);
+        return null;
+    }
+}
+
+app.on('ready', async function () {
+
+    const config = readConfigFile();
+    console.log('config:', config);
+    const teamOneLogo = storeImageAsVariable(path.join(__dirname, config.teamOneLogo));
+    const teamTwoLogo = storeImageAsVariable(path.join(__dirname, config.teamTwoLogo));
+    const teamOneName = config.teamOne;
+    const teamTwoName = config.teamTwo;
+    const teamOneStartingSide = config.teamOneStartingSide;
+    //create instance of interface ConfigData
+    const configData = {
+        teamOneLogo: await teamOneLogo,
+        teamOneName,
+        teamTwoLogo: await teamTwoLogo,
+        teamTwoName,
+        teamOneStartingSide,
+    }
+
+
     // Start express app
     const reactApp = express();
     reactApp.use('/', express.static(path.join(__dirname, './build'))); // Serve your static React build
@@ -15,6 +58,10 @@ app.on('ready', function () {
     // Create new browser window
     mainWindow = new BrowserWindow({
         show: false, // This will hide the electron browser window
+        webPreferences: {
+            contextIsolation: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     mainWindow.on('closed', function () {
