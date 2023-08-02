90908997aa83503e4185688c8bd02ad1aa92d42c
Zachary <tysonzachary@ufl.edu>
2023-08-01 20:40:08 -0400

    works in dev but not upon building

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,110 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { contextBridge, ipcRenderer } = require('electron')
+
 const path = require('path');
 const url = require('url');
 const express = require('express');
 const fs = require('fs');
+// Import parts of electron to use
+const {app, BrowserWindow} = require('electron');
 
-let server;
+//
+// let server;
+// let mainWindow;
+//
+//
+//
+// async function readConfigFile() {
+//     try {
+//         const configFileContent = fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8');
+//         const config = JSON.parse(configFileContent);
+//         return config;
+//     } catch (err) {
+//         console.error('Error reading config file:', err);
+//         return null;
+//     }
+// }
+//
+// async function storeImageAsVariable(filePath) {
+//     try {
+//         const imageData= await fs.promises.readFile(filePath);
+//         return imageData.toString('base64');
+//     } catch (error) {
+//         console.error('Error reading the image file:', error);
+//         return null;
+//     }
+// }
+//
+// async function getConfigData() {
+//     let config = await readConfigFile();
+//     const teamOneLogo = storeImageAsVariable(path.join(__dirname, config.teamOneLogo));
+//     const teamTwoLogo = storeImageAsVariable(path.join(__dirname, config.teamTwoLogo));
+//     const teamOneName = config.teamOne;
+//     const teamTwoName = config.teamTwo;
+//     const teamOneStartingSide = config.teamOneStartingSide;
+//     const ConfigData = {
+//         teamOneLogo: await teamOneLogo,
+//         teamOneName,
+//         teamTwoLogo: await teamTwoLogo,
+//         teamTwoName,
+//         teamOneStartingSide,
+//     };
+//     return ConfigData;
+// }
+//
+// app.on('ready', async function () {
+//
+//     // Start express app
+//     const reactApp = express();
+//     reactApp.use('/', express.static(path.join(__dirname, './build'))); // Serve your static React build
+//     server = reactApp.listen(3000, () => console.log('React App on localhost:3000'));
+//
+//     // Create new browser window
+//     mainWindow = new BrowserWindow({
+//         width: 1024,
+//         height: 768,
+//         webPreferences: {
+//             nodeIntegration: true,
+//             contextIsolation: true,
+//             preload: path.join(__dirname, './preload.js'),
+//         }
+//     });
+//
+//     mainWindow.on('closed', function () {
+//         mainWindow = null;
+//         server.close(); // Close express app when electron app is closed
+//     });
+// });
+//
+// app.on('window-all-closed', function () {
+//     if (process.platform !== 'darwin') app.quit();
+// });
+//
+// app.on('activate', function () {
+//     if (mainWindow === null) createWindow();
+// });
+//
+// app.whenReady().then(() => {
+//     /* Here is where you send the message which you have to recive from
+//        your preload file, the first arg is the name by which you will\
+//        identify it and the second is the message itself */
+//     ipcMain.handle('config', getConfigData);
+//     app.on('activate', function () {
+//         if (BrowserWindow.getAllWindows().length === 0) mainWindow()
+//     })
+// })
+
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+// Keep a reference for dev mode
+let dev = false;
+if ( process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath) ) {
+    dev = true;
+}
 
-
-function readConfigFile() {
+async function readConfigFile() {
     try {
         const configFileContent = fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8');
         const config = JSON.parse(configFileContent);
@@ -31,49 +125,101 @@ async function storeImageAsVariable(filePath) {
     }
 }
 
-app.on('ready', async function () {
-
-    const config = readConfigFile();
-    console.log('config:', config);
+async function getConfigData() {
+    let config = await readConfigFile();
     const teamOneLogo = storeImageAsVariable(path.join(__dirname, config.teamOneLogo));
     const teamTwoLogo = storeImageAsVariable(path.join(__dirname, config.teamTwoLogo));
     const teamOneName = config.teamOne;
     const teamTwoName = config.teamTwo;
     const teamOneStartingSide = config.teamOneStartingSide;
-    //create instance of interface ConfigData
-    const configData = {
+    const ConfigData = {
         teamOneLogo: await teamOneLogo,
         teamOneName,
         teamTwoLogo: await teamTwoLogo,
         teamTwoName,
         teamOneStartingSide,
-    }
+    };
+    return ConfigData;
+}
 
-
-    // Start express app
-    const reactApp = express();
-    reactApp.use('/', express.static(path.join(__dirname, './build'))); // Serve your static React build
-    server = reactApp.listen(3000, () => console.log('React App on localhost:3000'));
-
-    // Create new browser window
+function createWindow() {
+    // Create the browser window.
     mainWindow = new BrowserWindow({
-        show: false, // This will hide the electron browser window
+        width: 1024,
+        height: 768,
         webPreferences: {
+            nodeIntegration: true,
             contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, './preload.js'),
         }
     });
 
-    mainWindow.on('closed', function () {
+    // and load the index.html of the app.
+    let indexPath;
+    const reactApp = express();
+    reactApp.use('/', express.static(path.join(__dirname, './build'))); // Serve your static React build
+    let server = reactApp.listen(3000, () => console.log('React App on localhost:3000'));
+    if ( dev && process.argv.indexOf('--noDevServer') === -1 ) {
+        indexPath = url.format({
+            protocol: 'http:',
+            host: 'localhost:3000',
+            pathname: 'index.html',
+            slashes: true
+        });
+    } else {
+        indexPath = url.format({
+            protocol: 'file:',
+            pathname: path.join(__dirname, 'build', 'index.html'),
+            slashes: true
+        });
+    }
+    mainWindow.loadURL( indexPath );
+
+
+    // Don't show until we are ready and loaded
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+        getConfigData().then((configData) => {
+            mainWindow.webContents.send('channel', configData);
+        }).catch((error) => {
+            console.error('An error occurred:', error);
+        });
+
+        // Open the DevTools automatically if developing
+        if ( dev ) {
+            mainWindow.webContents.openDevTools();
+        }
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
         mainWindow = null;
         server.close(); // Close express app when electron app is closed
+
     });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
-});
-
-app.on('activate', function () {
-    if (mainWindow === null) createWindow();
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,6 @@ import Teams from "./teams/Teams";
 import CurrentPlayer from "./current_player/current_player";
 import {teamOneLogo, teamOneName, teamTwoLogo, teamTwoName, teamOneStartingSide} from "./teamInfo.js"
 import {ConfigData} from "./config_interface";
-import * as electron from "electron";
-const ipcRenderer = electron.ipcRenderer
 
 const ENDPOINT = "http://localhost:25566"; // replace with your server's address and port
 const backupTeamOneLogo = require("./config/teamOneLogo.png");
@@ -31,34 +29,49 @@ const App: React.FC = () => {
             socket.disconnect();
         }
     }, []);
+    // useEffect(() => {
+    //     // Read the config data from window.electronConfig
+    //     let electronConfig:any = null;
+    //
+    //     if (electronConfig) {
+    //         setConfig(electronConfig);
+    //         let imageSrc = 'data:image/png;base64,' + config?.teamOneLogo;
+    //         let imageSrc2 = 'data:image/png;base64,' + config?.teamTwoLogo;
+    //         if(config) {
+    //             config.teamOneLogo = imageSrc;
+    //             config.teamTwoLogo = imageSrc2;
+    //         }
+    //     }
+    //     // If the config data is not available, use the backup data
+    //     if(!config) {
+    //         const configData = backupConfig();
+    //         setConfig(configData);
+    //
+    //     }
+    // }, []);
+    // if (!response || !config) return <div>Loading...</div>;
+    // useEffect(() => {
+    //
+    // }, []);
     useEffect(() => {
-        ipcRenderer.on('config', (event:any, arg:any) => {
-            console.log(arg) // prints "Hello from renderer process"
-        })
-    },[])
-    useEffect(() => {
-        // Read the config data from window.electronConfig
-        let electronConfig:any = null;
-
-        if (electronConfig) {
-            setConfig(electronConfig);
-            let imageSrc = 'data:image/png;base64,' + config?.teamOneLogo;
-            let imageSrc2 = 'data:image/png;base64,' + config?.teamTwoLogo;
-            if(config) {
-                config.teamOneLogo = imageSrc;
-                config.teamTwoLogo = imageSrc2;
-            }
-        }
-        // If the config data is not available, use the backup data
-        if(!config) {
-            const configData = backupConfig();
-            setConfig(configData);
-
-        }
+        const electron = (window as any).electron;
+        electron.receive('channel', (data:any) => {
+            console.log(data); // prints { foo: 'bar' }
+            //convert base64 to image
+            let imageSrc = 'data:image/png;base64,' + data.teamOneLogo;
+            let imageSrc2 = 'data:image/png;base64,' + data.teamTwoLogo;
+            data.teamOneLogo = imageSrc;
+            data.teamTwoLogo = imageSrc2;
+            setConfig(data);
+        });
     }, []);
-    if (!response || !config) return <div>Loading...</div>;
+
+    // electron.receive('receive', (data:any) => {
+    //     console.log(data); // prints { foo: 'bar' }
+    // });
     return (
         <div>
+            hello
             <Scoreboard data={response} config={config}/>
             <Teams data={response} config={config}/> {/* Pass the object as a prop */}
             <CurrentPlayer data={response} config={config}/> {/* Pass the object as a prop */}
