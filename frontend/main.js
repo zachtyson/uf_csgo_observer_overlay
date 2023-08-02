8b4193760f426734dedf6bd1b36a0ec4f762482f
Zachary <tysonzachary@ufl.edu>
2023-08-02 19:31:11 -0400

    removed IPC code from main.js

--- frontend/main.js
+++ frontend/main.js
@@ -8,109 +8,22 @@ const {app, BrowserWindow} = require('electron');
 
 
 let mainWindow;
+let server;
 
-let dev = false;
-if ( process.defaultApp || /[\\/]electron-prebuilt[\\/]/.test(process.execPath) || /[\\/]electron[\\/]/.test(process.execPath) ) {
-    dev = true;
-}
-
-async function readConfigFile() {
-    try {
-        const configFileContent = fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8');
-        const config = JSON.parse(configFileContent);
-        return config;
-    } catch (err) {
-        console.error('Error reading config file:', err);
-        return null;
-    }
-}
-
-async function storeImageAsVariable(filePath) {
-    try {
-        const imageData= await fs.promises.readFile(filePath);
-        return imageData.toString('base64');
-    } catch (error) {
-        console.error('Error reading the image file:', error);
-        return null;
-    }
-}
-
-async function getConfigData() {
-    let config = await readConfigFile();
-    const teamOneLogo = storeImageAsVariable(path.join(__dirname, config.teamOneLogo));
-    const teamTwoLogo = storeImageAsVariable(path.join(__dirname, config.teamTwoLogo));
-    const teamOneName = config.teamOne;
-    const teamTwoName = config.teamTwo;
-    const teamOneStartingSide = config.teamOneStartingSide;
-    const ConfigData = {
-        teamOneLogo: await teamOneLogo,
-        teamOneName,
-        teamTwoLogo: await teamTwoLogo,
-        teamTwoName,
-        teamOneStartingSide,
-    };
-    return ConfigData;
-}
 
 function createWindow() {
-
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: true,
-            preload: path.join(__dirname, './preload.js'),
-        }
-    });
-
-    // and load the index.html of the app.
-    let indexPath;
-    let server;
     const reactApp = express();
     reactApp.use('/', express.static(path.join(__dirname, './build'))); // Serve your static React build
     server = reactApp.listen(3000, () => console.log('React App on localhost:3000'));
-    if ( dev && process.argv.indexOf('--noDevServer') === -1 ) {
-        indexPath = url.format({
-            protocol: 'http:',
-            host: 'localhost:3000',
-            pathname: 'index.html',
-            slashes: true
-        });
-    } else {
-        indexPath = url.format({
-            protocol: 'file:',
-            pathname: path.join(__dirname, 'build', 'index.html'),
-            slashes: true
-        });
-    }
-    mainWindow.loadURL( indexPath );
 
-
-    // Don't show until we are ready and loaded
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-        getConfigData().then((configData) => {
-            mainWindow.webContents.send('channel', configData);
-        }).catch((error) => {
-            console.error('An error occurred:', error);
-        });
-
-        // Open the DevTools automatically if developing
-        if ( dev ) {
-            mainWindow.webContents.openDevTools();
-        }
+    // Create new browser window
+    mainWindow = new BrowserWindow({
+        show: false, // This will hide the electron browser window
     });
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
+    mainWindow.on('closed', function () {
         mainWindow = null;
         server.close(); // Close express app when electron app is closed
-
     });
 }
 
