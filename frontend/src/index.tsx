f8bbcfd32ac2323c56caa29b39153f27c22c3740
Zachary <tysonzachary@ufl.edu>
2023-08-02 19:33:12 -0400

    Added code for passing config via index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import App from "./App";
+import { ConfigData } from "./config_interface";
+
+const ENDPOINT = "http://localhost:25566/"; // replace with your server's address and port
+
+console.log(`${process.env.REACT_APP_PUBLIC_URL}`);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function AppWrapper() {
+    const [appConfiguration, setAppConfiguration] = useState<ConfigData | null>(null);
+
+    useEffect(() => {
+        axios.get(ENDPOINT)
+            .then((response: any) => {
+                console.log('Response:', response.data);
+                // Assuming the response data is of type ConfigData, update the state
+                setAppConfiguration(response.data);
+            })
+            .catch((error: any) => {
+                console.error('Error:', error.message);
+            });
+    }, []);
+
+    // Render the App component only when appConfiguration is available
+    if (appConfiguration) {
+        return (
+            <React.StrictMode>
+                <App appConfiguration={appConfiguration} />
+            </React.StrictMode>
+        );
+    } else {
+        // Render a loading or fallback UI while waiting for the response
+        return <div>Loading...</div>;
+    }
+}
+
+root.render(<AppWrapper />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
