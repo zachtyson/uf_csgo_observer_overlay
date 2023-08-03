c6e07a374e85682637c4a63213526b6c6fd62bb4
Zachary <tysonzachary@ufl.edu>
2023-08-03 18:51:06 -0400

    changing require did not work lol

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 const express = require("express");
 const fs = require("fs");
-const config = require("../../config.json");
+const config = require("../config.json");
 const log = require("simple-node-logger").createSimpleFileLogger("csgo-gamestate.log");
 const http = require("http");
 const socketIo = require("socket.io");
