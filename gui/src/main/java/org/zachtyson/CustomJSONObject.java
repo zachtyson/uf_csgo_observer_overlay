22ecdd54326d3138ce3eeabab46709e007432561
Zachary <tysonzachary@ufl.edu>
2023-08-08 18:25:58 -0400

    Converted to maven project

--- gui/src/main/java/org/zachtyson/CustomJSONObject.java
+++ gui/src/main/java/org/zachtyson/CustomJSONObject.java
@@ -1,3 +1,5 @@
+package main.java.org.zachtyson;
+
 import java.util.HashMap;
 import java.util.Map;
 
--- gui/src/main/java/org/zachtyson/Main.java
+++ gui/src/main/java/org/zachtyson/Main.java
@@ -1,3 +1,5 @@
+package main.java.org.zachtyson;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
