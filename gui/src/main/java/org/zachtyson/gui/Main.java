230a9f87718ab4a7a36b1bd379ea5a73ed3c22ba
Zachary <tysonzachary@ufl.edu>
2023-08-08 19:16:26 -0400

    Created classes for config

--- gui/src/main/java/org/zachtyson/gui/Main.java
+++ gui/src/main/java/org/zachtyson/gui/Main.java
@@ -13,6 +13,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 
 public class Main {
     private static final String EXE_FILE_1 = "frontend/frontend.exe";
@@ -467,5 +468,65 @@ public class Main {
             throw new RuntimeException("Unable to generate gamestate_integration_uf.cfg file", e);
         }
 
+    }
+
+    static class Config {
+        @SerializedName("application")
+        private ApplicationData application;
+
+        @SerializedName("team_data")
+        private TeamData teamData;
+
+        public Config(ApplicationData application, TeamData teamData) {
+            this.application = application;
+            this.teamData = teamData;
+        }
+    }
+
+    static class TeamData {
+        @SerializedName("teamOneName")
+        private String teamOneName;
+
+        @SerializedName("teamTwoName")
+        private String teamTwoName;
+
+        @SerializedName("teamOneLogo")
+        private String teamOneLogo;
+
+        @SerializedName("teamTwoLogo")
+        private String teamTwoLogo;
+
+        @SerializedName("teamOneStartingSide")
+        private String teamOneStartingSide;
+
+        @SerializedName("bombTime")
+        private int bombTime;
+
+        public TeamData(String teamOneName, String teamTwoName, String teamOneLogo, String teamTwoLogo, String teamOneStartingSide, int bombTime) {
+            this.teamOneName = teamOneName;
+            this.teamTwoName = teamTwoName;
+            this.teamOneLogo = teamOneLogo;
+            this.teamTwoLogo = teamTwoLogo;
+            this.teamOneStartingSide = teamOneStartingSide;
+            this.bombTime = bombTime;
+        }
+
+    }
+
+    static class ApplicationData {
+        @SerializedName("logLevel")
+        private String logLevel;
+
+        @SerializedName("port")
+        private int port;
+
+        @SerializedName("host")
+        private String host;
+
+        public ApplicationData(String logLevel, int port, String host) {
+            this.logLevel = logLevel;
+            this.port = port;
+            this.host = host;
+        }
     }
 }
