80440056b78279f7a070393edc9448ffb5caac73
Zachary <tysonzachary@ufl.edu>
2023-08-24 15:24:28 -0400

    adding listener for selecitng file

--- gui/src/main/java/com/zachtyson/gui/MainController.java
+++ gui/src/main/java/com/zachtyson/gui/MainController.java
@@ -6,7 +6,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.Base64;
 import java.util.ResourceBundle;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.IOException;
 
 public class MainController implements Initializable {
     @FXML
@@ -39,10 +44,33 @@ public class MainController implements Initializable {
             tab.setClosable(false);
         }
     }
+    @FXML
+    public Button teamOneLogoButton;
+    @FXML
+    public Button teamTwoLogoButton;
 
+    @FXML
     public void handleTeamOneLogoSelection(ActionEvent actionEvent) {
+        selectLogo(teamOneLogoButton);
     }
 
+    @FXML
     public void handleTeamTwoLogoSelection(ActionEvent actionEvent) {
+        selectLogo(teamTwoLogoButton);
+    }
+
+    private void selectLogo(Button button) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            try {
+                // Note: getBase64(...) method is not provided in the given code, so this is a placeholder
+                String base64Logo = getBase64(selectedFile);
+                button.setText(selectedFile.getName());
+                // Optionally, you can store the base64 representation in a variable or use it further.
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
