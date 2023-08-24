94edd63aefe371694ac73340a9f103010e8e378d
Zachary <tysonzachary@ufl.edu>
2023-08-24 16:21:24 -0400

    StartTabController should be done

--- gui/src/main/java/com/zachtyson/gui/StartTabController.java
+++ gui/src/main/java/com/zachtyson/gui/StartTabController.java
@@ -1,14 +1,15 @@
 package com.zachtyson.gui;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -27,8 +28,8 @@ public class StartTabController implements Initializable {
     public Button backendButton;
     @FXML
     public TextArea outputArea1;
-    private AtomicBoolean isFrontendRunning = new AtomicBoolean(false);
-    private AtomicBoolean isBackendRunning = new AtomicBoolean(false);
+    private final AtomicBoolean isFrontendRunning = new AtomicBoolean(false);
+    private final AtomicBoolean isBackendRunning = new AtomicBoolean(false);
 
     private void executeProcess(String fileName, int processNumber) {
         File exeFile = new File(fileName);
@@ -88,22 +89,51 @@ public class StartTabController implements Initializable {
         stage.setOnCloseRequest(event -> {
             if (process1 != null) {
                 process1.destroy();
+                process1 = null;
             }
             if (process2 != null) {
                 process2.destroy();
+                process2 = null;
             }
             if (processThread1 != null && processThread1.isAlive()) {
                 processThread1.interrupt();
+                processThread1 = null;
             }
             if (processThread2 != null && processThread2.isAlive()) {
                 processThread2.interrupt();
+                processThread2 = null;
             }
             System.exit(0);
         });
     }
 
+    private void toggleProcess(String fileName, int processNumber) {
+        if (processNumber == 1) {
+            if (isFrontendRunning.get()) {
+                stopProcess(1);
+                isFrontendRunning.set(false);
+                frontendButton.setText("Start Frontend");
+            } else {
+                executeProcess(fileName, 1);
+                isFrontendRunning.set(true);
+                frontendButton.setText("Stop Frontend");
+            }
+        } else if (processNumber == 2) {
+            if (isBackendRunning.get()) {
+                stopProcess(2);
+                isBackendRunning.set(false);
+                backendButton.setText("Start Backend");
+            } else {
+                executeProcess(fileName, 2);
+                isBackendRunning.set(true);
+                backendButton.setText("Stop Backend");
+            }
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        frontendButton.setOnAction(event -> toggleProcess(EXE_FILE_1, 1));
+        backendButton.setOnAction(event -> toggleProcess(EXE_FILE_2, 2));
     }
 }
