659e047caab53c7cb027b96e29218290c98fd59f
Zachary <tysonzachary@ufl.edu>
2023-08-24 17:49:07 -0400

    Added bootstrapfx

--- gui/src/main/java/com/zachtyson/gui/Main.java
+++ gui/src/main/java/com/zachtyson/gui/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
@@ -17,7 +18,9 @@ public class Main extends Application {
         StartTabController startTabController = new StartTabController();
         startTabController.setCloseBehavior(stage);
         Scene scene = new Scene(root, 800, 800);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+
         stage.setTitle("CSGO Observer Overlay GUI");
         stage.setScene(scene);
         stage.show();
