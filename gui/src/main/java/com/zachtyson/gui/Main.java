4e29afac00774272a34e8c464f1b3678e50cc597
Zachary <tysonzachary@ufl.edu>
2023-08-24 15:19:50 -0400

    renamed hello-view.fxml

--- gui/src/main/java/com/zachtyson/gui/Main.java
+++ gui/src/main/java/com/zachtyson/gui/Main.java
@@ -45,10 +45,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
-        stage.setTitle("Hello!");
+        stage.setTitle("CSGO Observer Overlay GUI");
         stage.setScene(scene);
         stage.show();
     }
