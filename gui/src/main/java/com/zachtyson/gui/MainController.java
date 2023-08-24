b999e5843ee876264293b4c6a61afd2341d617b1
Zachary <tysonzachary@ufl.edu>
2023-08-24 15:56:29 -0400

    moving everything out of MainController

--- gui/src/main/java/com/zachtyson/gui/MainController.java
+++ gui/src/main/java/com/zachtyson/gui/MainController.java
@@ -17,39 +17,11 @@ import java.io.IOException;
 
 public class MainController implements Initializable {
     @FXML
-    public Button frontendButton;
-    @FXML
-    public Button backendButton;
-    @FXML
     public TabPane tabPane;
     @FXML
-    public TextArea outputArea1;
-    @FXML
-    public TextArea outputArea3;
-    @FXML
-    public Button importConfig;
-    @FXML
-    public Button gameLocationButton;
-    @FXML
-    public Button cfgButton;
-    @FXML
-    public TextField teamOneNameField;
-    @FXML
-    public TextField teamTwoNameField;
-    @FXML
-    public TextField bombTimerField;
-    @FXML
-    public Button createButton;
-    @FXML
-    public TextArea outputArea2;
-    public ComboBox teamOneStartingSideComboBox;
-
-    @FXML
     private Tab startTab;
-
     @FXML
     private Tab settingsTab;
-
     @FXML
     private Tab gameLocationTab;
     @Override
@@ -65,52 +37,5 @@ public class MainController implements Initializable {
             e.printStackTrace();
         }
 
-    }
-    @FXML
-    public Button teamOneLogoButton;
-    @FXML
-    public Button teamTwoLogoButton;
-
-    @FXML
-    public void handleTeamOneLogoSelection(ActionEvent actionEvent) {
-        selectLogo(teamOneLogoButton);
-    }
-
-    @FXML
-    public void handleTeamTwoLogoSelection(ActionEvent actionEvent) {
-        selectLogo(teamTwoLogoButton);
-    }
-
-    private void selectLogo(Button button) {
-        FileChooser fileChooser = new FileChooser();
-        File selectedFile = fileChooser.showOpenDialog(null);
-        if (selectedFile != null) {
-            try {
-                String base64Logo = getBase64(selectedFile);
-                button.setText(selectedFile.getName());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    private String getBase64(File file) throws IOException {
-        byte[] fileBytes = Files.readAllBytes(file.toPath());
-        return "data:image/" + getFileExtension(file) + ";base64," + Base64.getEncoder().encodeToString(fileBytes);
-    }
-
-    public static String getFileExtension(File file) {
-        String name = file.getName();
-        int lastIndexOf = name.lastIndexOf(".");
-        if (lastIndexOf == -1) {
-            return ""; // Empty extension
-        }
-        return name.substring(lastIndexOf + 1).toLowerCase();
-    }
-
-    public void handleCreateJson(ActionEvent actionEvent) {
-    }
-
-    public void handleImportConfig(ActionEvent actionEvent) {
     }
 }
