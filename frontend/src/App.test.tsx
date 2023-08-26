c569e3fd7265ac6c04fd627fde8473a41b061a07
Zachary <tysonzachary@ufl.edu>
2023-08-26 17:54:00 -0400

    changed back

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-    render(<App appConfiguration={null} uiStyle={null} />);
+    render(<App appConfiguration={null} />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
 });
