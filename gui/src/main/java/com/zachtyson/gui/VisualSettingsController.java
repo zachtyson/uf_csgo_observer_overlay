384f0ab970b811655b1d00c05989ae24c60aff34
Zachary <tysonzachary@ufl.edu>
2023-08-27 14:19:39 -0400

    added 5 color pickers

--- gui/src/main/java/com/zachtyson/gui/VisualSettingsController.java
+++ gui/src/main/java/com/zachtyson/gui/VisualSettingsController.java
@@ -1,11 +1,23 @@
 package com.zachtyson.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ColorPicker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VisualSettingsController implements Initializable {
+    @FXML
+    public ColorPicker colorPicker1;
+    @FXML
+    public ColorPicker colorPicker2;
+    @FXML
+    public ColorPicker colorPicker3;
+    @FXML
+    public ColorPicker colorPicker4;
+    @FXML
+    public ColorPicker colorPicker5;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
