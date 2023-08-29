831988513008e33bc077adfae3a8dea93dd7b4f0
Zachary <tysonzachary@ufl.edu>
2023-08-28 20:49:32 -0400

    moved importConfig

--- gui/src/main/java/com/zachtyson/gui/ConfigData.java
+++ gui/src/main/java/com/zachtyson/gui/ConfigData.java
@@ -1,27 +1,37 @@
 package com.zachtyson.gui;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.SerializedName;
+import java.io.File;
+import java.io.FileReader;
+
+import static com.zachtyson.gui.Main.writeJsonToFile;
 
 public class ConfigData {
     @SerializedName("application")
-    public ApplicationData application;
+    public ApplicationData application = new ApplicationData("info", 25566, "127.0.0.1");
 
     @SerializedName("team_data")
     public TeamData teamData;
 
-    private ConfigData(ApplicationData application, TeamData teamData) {
-        this.application = application;
-        this.teamData = teamData;
+    private ConfigData(ApplicationData application, TeamData teamData, UIColorsData uiColorsData) {
+
     }
 
-    public static ConfigData getInstance(ApplicationData application, TeamData teamData) {
+    @SerializedName("ui_colors")
+    public UIColorsData uiColorsData;
+
+    public static ConfigData getInstance(ApplicationData application, TeamData teamData, UIColorsData uiColorsData) {
         if (singleInstance == null) {
-            singleInstance = new ConfigData(application, teamData);
+            singleInstance = new ConfigData(application, teamData, uiColorsData);
         }
-        if(application == null) {
+        if(application != null) {
             singleInstance.application = application;
         }
-        if(teamData == null) {
+        if(teamData != null) {
             singleInstance.teamData = teamData;
         }
         return singleInstance;
@@ -44,4 +54,30 @@ public class ConfigData {
     }
 
     public static ConfigData singleInstance = null;
+
+    public void importConfig(File f) throws RuntimeException{
+        if (f != null) {
+            try (FileReader reader = new FileReader(f)) {
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                ConfigData configObj = gson.fromJson(reader, ConfigData.class);
+                String newConfigObj = gson.toJson(configObj);
+                writeJsonToFile(newConfigObj, "config.json");
+                writeJsonToFile(newConfigObj, "backend/config.json");
+            } catch (JsonSyntaxException ex) {
+                System.out.println("Error: Invalid JSON syntax.");
+                throw new RuntimeException("Error: Invalid JSON syntax.");
+            } catch (JsonIOException ex) {
+                System.out.println("Error: Invalid JSON IO.");
+                throw new RuntimeException("Error: Failed to read JSON.");
+            } catch (NullPointerException ex) {
+                // User opened file chooser but didn't select a file
+                System.out.println("Error: No file selected.");
+            } catch (Exception ex) {
+                System.out.println("Error: Failed to import config.");
+                throw new RuntimeException("Error: Failed to import config.");
+            }
+        } else {
+            System.out.println("Error: No file selected.");
+        }
+    }
 }
--- gui/src/main/java/com/zachtyson/gui/SettingsController.java
+++ gui/src/main/java/com/zachtyson/gui/SettingsController.java
@@ -65,7 +65,7 @@ public class SettingsController implements Initializable {
             int gameLength = Integer.parseInt(gameLengthField.getText());
             int overtimeHalfLength = Integer.parseInt(overtimeLengthField.getText());
             TeamData teamData = new TeamData(teamOneName, teamTwoName, teamOneLogo, teamTwoLogo, teamOneStartingSide, bombTime, halfLength, overtimeHalfLength, gameLength);
-            ConfigData configData = ConfigData.getInstance(applicationData, teamData);
+            ConfigData configData = ConfigData.getInstance(applicationData, teamData,null);
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             gson.toJson(configData);
             writeJsonToFile(gson.toJson(configData), "backend/config.json"); //this one doesn't work but the bottom one doesn't work unless this one is here
@@ -125,32 +125,9 @@ public class SettingsController implements Initializable {
     }
 
     public void handleImportConfig() {
+        ConfigData configData = ConfigData.getInstance(null, null,null);
         FileChooser importConfigChooser = new FileChooser();
         File selectedGameConfig = importConfigChooser.showOpenDialog(null);
-        if (selectedGameConfig != null) {
-            importConfig.setText(selectedGameConfig.getName());
-
-            try (FileReader reader = new FileReader(selectedGameConfig)) {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                ConfigData configObj = gson.fromJson(reader, ConfigData.class);
-                String newConfigObj = gson.toJson(configObj);
-                writeJsonToFile(newConfigObj, "config.json");
-            } catch (JsonSyntaxException ex) {
-                System.out.println("Error: Invalid JSON syntax.");
-                ImportConfigOutputArea.setText("Error: Invalid JSON syntax.");
-            } catch (JsonIOException ex) {
-                System.out.println("Error: Invalid JSON IO.");
-                ImportConfigOutputArea.setText("Error: Failed to read JSON.");
-            } catch (NullPointerException ex) {
-                // User opened file chooser but didn't select a file
-                System.out.println("Error: No file selected.");
-            } catch (Exception ex) {
-                System.out.println("Error: Failed to import config.");
-                ImportConfigOutputArea.setText("Error: Failed to import config.");
-                ex.printStackTrace();
-            }
-        } else {
-            System.out.println("Error: No file selected.");
-        }
+        configData.importConfig(selectedGameConfig);
     }
 }
